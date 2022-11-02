98b4b74e00359388fa119be0e811a2a7dade870d
nodev918 <nodev918@gmail.com>
2022-11-02 12:56:57 +0800

    add comment

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { counter } from './counter.js'
 import { store } from './redux.js'
 
+// 這邊要把store傳進去給counter用
 counter(store)
 
+// store.subscribe: 為了dispatch的時候, 可以重新設定innerHTML
 store.subscribe(()=>{
   counter(store)
 })
--- redux.js
+++ redux.js
@@ -1,4 +1,4 @@
-
+// 這邊可以設定各種dispatch的情況
 function reducerFn(state = 0, action) {
     return action?.type == "add"
     ? state + action.payload
@@ -7,16 +7,20 @@ function reducerFn(state = 0, action) {
     : state;
 }
 
+// store的核心class, 包含一個reducer() return出來的state作為data member, 閉包的方式maintain
 function createStore(reducer) {
     let state = reducer();
     let eventhub = [];
+
     function getState() {
         return state;
     }
+
     function dispatch(action) {
     state = reducer(state, action);
         eventhub.forEach((i) => i());
     }
+
     function subscribe(fn) {
         eventhub.push(fn);
     }
@@ -27,7 +31,10 @@ function createStore(reducer) {
         subscribe,
     };
 }
+
+// 把store這個物件return出去讓大家共用, 以維護全域狀態
 let store = createStore(reducerFn)
+
 export {
     reducerFn,
     createStore,
